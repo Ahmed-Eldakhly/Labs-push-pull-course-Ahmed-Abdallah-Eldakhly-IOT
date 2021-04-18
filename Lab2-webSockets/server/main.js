db52086378e4303eba5c8985e0e99c3b50e5c8e6
Ahmed-Eldakhly <ahmedeldakhlly94@gmail.com>
2021-04-19 01:21:32 +0200

    web-socket-lab2

--- Lab2-webSockets/server/main.js
+++ Lab2-webSockets/server/main.js
@@ -1,40 +1,27 @@
-const express = require('express')
-const cors = require('cors')
 
-const app = express()
-app.use(express.json());
-app.use(cors())
+const http = require('http');
 
-let messagess = [];
+const server = http.createServer()
 
-app.listen(4000, () => console.log('Server running on port 4000!'))
-
-app.post('/messages' , (req , res) => {
-
-   //console.log(req.body)
-    messagess.push({data: req.body.data , lastTime: req.body.lastTime});
-    // console.log(req.body.data)
-     console.log(messagess)
-    res.status(200).end();
+const io = require('socket.io')(server , {
+  cors: {
+    origin: '*',
+    methods: '*'
+  }
 });
 
-app.get('/messages' , (req , res) => {
-  // console.log(req.query.date)
-  let userDate = req.query.lastTime;
-  console.log(userDate)
-  if(userDate === 0)
-    res.json(messagess)
-  else
-  {
-    console.log("the array" , messagess)
-    console.log("the value",userDate)
-    const response =  messagess.filter((message) => {
-      console.log(message.lastTime)
-      if(message.lastTime > userDate){
-      console.log(message.lastTime)
-        return message}
-    })
-    res.json(response)
-    console.log(response)
-  }
-  })
+const PORT = 3001
+
+
+io.on('connection' , (socket) => {
+  console.log('New Connection' , socket.id)
+  socket.on('message'  , (message) => {
+    console.log(message)
+    socket.broadcast.emit('new-message' , message)
+  })})
+
+
+server.listen(3001 , (err) => {
+  console.info('Server Listening on port 3001')
+});
+

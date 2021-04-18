7df68833b688dd8b89ed29b881ef8851eeda8843
Ahmed-Eldakhly <ahmedeldakhlly94@gmail.com>
2021-04-18 17:02:17 +0200

    short pulling

--- short-pulling/server/main.js
+++ short-pulling/server/main.js
@@ -5,19 +5,36 @@ const app = express()
 app.use(express.json());
 app.use(cors())
 
-const messagess = [];
+let messagess = [];
 
 app.listen(4000, () => console.log('Server running on port 4000!'))
 
 app.post('/messages' , (req , res) => {
-  console.log(req.body)
-    messagess.push(req.body);
-    console.log(req.body.data)
-    console.log(messagess)
+
+   //console.log(req.body)
+    messagess.push({data: req.body.data , lastTime: req.body.lastTime});
+    // console.log(req.body.data)
+     console.log(messagess)
     res.status(200).end();
 });
 
 app.get('/messages' , (req , res) => {
-    console.log(res)
-    res.json({messagess})
+  // console.log(req.query.date)
+  let userDate = req.query.lastTime;
+  console.log(userDate)
+  if(userDate === 0)
+    res.json(messagess)
+  else
+  {
+    console.log("the array" , messagess)
+    console.log("the value",userDate)
+    const response =  messagess.filter((message) => {
+      console.log(message.lastTime)
+      if(message.lastTime > userDate){
+      console.log(message.lastTime)
+        return message}
+    })
+    res.json(response)
+    console.log(response)
+  }
   })
